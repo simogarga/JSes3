8a8aac18a9a7d325a42d1ee18f8e0255aa592b24
Gargano Simone <gargano.simone@osdbsesto>
2020-12-10 13:38:18 +0100

    ultimo

--- js.js
+++ js.js
@@ -1,17 +1,42 @@
-function add(){
-    var num = $(#numero1)
+
+$("#btn1").click(function () {
+  calcola("+");
+});
+$("#btn2").click(function () {
+  calcola("-");
+});
+$("#btn3").click(function () {
+  calcola("/");
+});
+$("#btn4").click(function () {
+  calcola("*");
+});
+
+function calcola(segno) {
+  var n1 = parseInt($("#num1").val());
+  var n2 = parseInt($("#num2").val());
+  var risultato;
+
+
+
+  
+  switch (segno) {
+    case "+":
+      risultato = n1 + n2;
+      break;
+    case "-":
+      risultato = n1 - n2;
+      break;
+    case "/":
+      risultato = n1 / n2;
+      break;
+    case "*":
+      risultato = n1 * n2;
+      break;
+  }
+  $("#span1").html(risultato);
+  $("#tabella").append("<tr id=riga><td>"+n1+"</td><td>"+segno+"</td><td>"+n2+"</td><td>=</td><td>"+risultato+"</td><td><button id=btn>Elimina</button> </td></tr>");
+  $("#btn").click(function(){
+   $("#riga").remove();
+  })
 }
-
-
-function sott(){
-
-}
-
-function molt(){
-
-}
-
-function div(){
-
-}
-
